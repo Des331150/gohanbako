fcd9ee00cbfb184b397c8afadb9f2ebf7e3c1e65
Desmond Papa Arhinful <arhinfuldesmondpapa02@gmail.com>
2025-05-12 00:24:57 +0000

    added the image processing logic in the models.py file

--- gohan_bako/gohan/models.py
+++ gohan_bako/gohan/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+from django.core.files.base import ContentFile
+from io import BytesIO
+import os
 
 # Create your models here.
 class Labels(models.Model):
@@ -11,11 +15,42 @@ class Labels(models.Model):
 class Recipes(models.Model):
     recipe_name = models.CharField(max_length=240)
     description = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to="recipes/")
+    image = models.ImageField(upload_to="recipes/", null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipes")
     publication_date = models.DateTimeField(auto_now_add=True)
     labels = models.ManyToManyField(Labels, related_name="recipes")
 
     def __str__(self):
         return self.recipe_name
+    
+    def save(self, *args, **kwargs):
+        if self.image:
+            try:
+                img = Image.open(self.image)
+                img.verify()
+                
+                img =  Image.open(self.image)
+
+                if img.mode in ("RGBA", "LA", "P"):
+                    img = img.convert("RGB")
+
+                new_width = 800
+                original_width, original_height = img.size
+                new_height = int((new_width / original_width) * original_height)
+
+                img = img.resize((new_width, new_height), Image.LANCZOS)   
+
+                temp_img = BytesIO()
+                img.save(temp_img, format="JPEG", quality=70, optimize=True)
+                temp_img.seek(0)
+
+                original_name, _ = os.path.splitext(self.image.name)
+                img = f"{original_name}.jpg"
+
+                self.image.save(img, ContentFile(temp_img.read()), save=False)
+            except (IOError, OSError) as e:
+                raise ValueError(f"This upload file is not a valid image. -- {e}")
+        super().save(*args, **kwargs)
+
+
 
--- gohan_bako/gohan/views.py
+++ gohan_bako/gohan/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User, Recipes, Labels
+from .serializer import LabelsSerializer, RecipesSerializer
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+@api_view(["GET"])
+def list_recipes(request):
+    recipes = Recipes.objects.all()
+    serializer = RecipesSerializer(recipes, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def retrieve_recipe(request, id):
+    try:
+        recipe = Recipes.objects.get(pk=id)
+    except Recipes.DoesNotExist:
+        return Response({"detail": "Recipe not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = RecipesSerializer(recipe)
+    return Response(serializer.data)
+    
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def create_recipe(request):
+    if request.method == "POST":
+        serializer = RecipesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
