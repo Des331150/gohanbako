f58299f2c01920f047b58f5a6d4a2233d450de64
Desmond Papa Arhinful <arhinfuldesmondpapa02@gmail.com>
2025-05-13 15:19:09 +0000

    wrote the urls for the views

--- gohan_bako/gohan/urls.py
+++ gohan_bako/gohan/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from rest_framework.authtoken import views
+from . import views
 
 urlpatterns = [
-    path("api/token-auth", views.obtain_auth_token)
-    
+    path("api/token-auth", views.obtain_auth_token),
+    path("gohan/create/",views.create_user, name="create_user"),
+    path("gohan/", views.list_recipes, name="list_recipes"),
+    path("gohan/<int:id>/", views.retrieve_recipe, name="retrieve_recipe"),
+    path("gohan/recipe/create/", views.create_recipe, name="create_recipe")
 ]
